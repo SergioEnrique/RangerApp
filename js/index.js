bc480316fd638f72bc07e75373645409a52efae2
anrai <anrai@me.com>
2014-05-13 18:37:18 -0500

    Detalles del menu

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ $('#map-page').live('pageshow', function(event) {
     var map = new GoogleMap();
     map.initialize();
 });
+
+// Cerrar sesión
+/*$('#logout').click(function() {
+    alert( "Gracias por usar IberoBus App" );
+    $.mobile.changePage("index.html", "slide");
+});*/
